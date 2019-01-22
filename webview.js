55f1c1b92120f3a81594db7d0da55fe5631836f4
Benjamin Coenen <benjamin.coenen@corp.ovh.com>
2019-01-22 15:59:06 +0100

    feat: add franz recipe for webex teams
    Signed-off-by: Benjamin Coenen <benjamin.coenen@corp.ovh.com>

--- webview.js
+++ webview.js
@@ -2,12 +2,14 @@ module.exports = (Franz) => {
   const getMessages = function getMessages() {
     let span = document.querySelectorAll('#container > nav > ul > li:nth-child(1) > div > button > span > span');
     if (!span || span.length === 0) {
+      Franz.setBadge(0);
       return null;
     }
     let count = parseInt(span[0].innerText, 10);
-    if (!Number.isNaN(count) || count > 0) {
-      Franz.setBadge(count);
+    if (Number.isNaN(count)) {
+      count = 0;
     }
+    Franz.setBadge(count);
   };
 
   Franz.loop(getMessages);
